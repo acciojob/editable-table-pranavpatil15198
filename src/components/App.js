363fa91e9ce4c6a645c3a24a5547a6c464ceb7af
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-04-21 11:54:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
 import React from "react";
 import './../styles/App.css';
+import EditableTable from "./EditableTable";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Track edited cells to log updates for future</h1>
+      <EditableTable />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
